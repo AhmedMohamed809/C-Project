bad2c1f04936f834274540487a615d4cab3b0667
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-05-09 00:26:07 +0100

    add int , char , float

--- C_learning.c
+++ C_learning.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-printf("Hi i'm Ahmed"); 
-printf("\n Kmonnn");
+    int age=10; 
+    char gander = 'M';
+    float y = 2.1;
+    char name[]="Ahmed";
+    printf("My name is  %s  \n",name);
+printf("I'm  %d years old  \n",age); 
+printf("My gander is %c \n",gander);
+printf("Your GBA is %f",y);
+
+
     return 0;
 }
