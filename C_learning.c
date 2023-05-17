23c3d23c040d6dd62698db337c6771cea3ba49d4
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-05-17 03:00:14 +0100

    add input &sacnf

--- C_learning.c
+++ C_learning.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int age=10; 
-    char gander = 'M';
-    float y = 2.1;
-    char name[]="Ahmed";
-    printf("My name is  %s  \n",name);
-printf("I'm  %d years old  \n",age); 
-printf("My gander is %c \n",gander);
-printf("Your GBA is %f",y);
-
+//     int age=10; 
+//     char gander = 'M';
+//     float y = 2.1;
+//     char name[]="Ahmed";
+// printf("My name is  %s  \n",name);
+// printf("I'm  %d years old  \n",age); 
+// printf("My gander is %c \n",gander);
+// printf("Your GBA is %f",y);
+//*************************************
+char name[25];
+int age ;
+printf(" \n what is your name  :");
+scanf("%s",&name);
+printf(" \n How old are you :");
+scanf("%d",&age);
+printf("your name is %s  and your are %d years old " ,name, age);
 
     return 0;
 }
